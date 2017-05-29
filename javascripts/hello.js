b4eb017030476fbbc624eefe29ab29f9e00a891a
arihito <arihito.m@gmail.com>
2017-05-29 19:33:00 +0900

    hifive first

---
+++ javascripts/hello.js
@@ -0,0 +1,14 @@
+$(function() {
+  // DOM構築完了
+
+  // コントローラの定義
+  var helloworldController = {
+    __name: 'HelloWorldController',
+    '#button click': function() {
+      alert("こんにちは世界");
+    }
+  };
+
+  // コントローラ化する
+  h5.core.controller('body', helloworldController);
+}); 
